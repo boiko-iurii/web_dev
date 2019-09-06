b61bc27f16be0e59330d6841836ed5f5fc12e529
Iurii <boiko.iurii86@gmail.com>
2019-09-06 23:38:42 +0300

    add lesson 4

---
+++ lessons/4_forms_textarea_range_checkbox/script.js
@@ -0,0 +1,20 @@
+let pass = document.querySelector('#pass');
+let btn1 = document.querySelector('#go-1');
+let output1 = document.querySelector('#output-1');
+
+let color = document.querySelector('#col');
+let btn2 = document.querySelector('#set');
+let setColor = document.querySelector('.set-color');
+let output2 = document.querySelector('#output-2');
+
+
+btn1.onclick = () => {
+    output1.innerText = pass.value;
+}
+
+
+
+btn2.onclick = () => {
+    setColor.style.backgroundColor = color.value;
+    output2.innerText = color.value;
+}
