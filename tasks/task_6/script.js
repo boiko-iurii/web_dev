4f604a52eeac4e17d2620e38d2fbe0c33aaded3f
Iurii <boiko.iurii86@gmail.com>
2019-10-08 09:12:14 +0300

    change at 6 unit

--- tasks/task_6/script.js
+++ tasks/task_6/script.js
@@ -159,6 +159,7 @@ for (let i = 1; i <= 3; i++) {
     output8.innerHTML += '<br>';
 
 }
+
 for (let i = 2; i >= 1; i--) {
 
     for (let k = 1; k <= i; k++) {
@@ -180,22 +181,291 @@ for (let i = 2; i >= 1; i--) {
 **                                                   ******
 **
 */
+for (let i = 1; i <= 5; i++) {
 
+    if (i == 1 || i == 5) {
 
-for (let k = 0; k < 5; k++) {
+        for (let k = 1; k <= 6; k++) {
 
-    output9.innerHTML += '*';
+            output9.innerHTML += '*';
 
-}
+        }
 
-for (let i = 0; i < 3; i++) {
+    } else {
+
+        for (let k = 1; k <= 6; k++) {
+
+            if (k == 1 || k == 6) {
+
+                output9.innerHTML += '*';
+
+            } else {
+
+                output9.innerHTML += '&nbsp;';
+
+            }
+
+        }
+    }
 
     output9.innerHTML += '<br>';
 
 }
 
-for (let k = 0; k < 5; k++) {
+/* Task 11
+**  С помощью вложенных циклов выведите таблицу умножения на 6 и 7.
+**
+*/
 
-    output9.innerHTML += '*';
+for (let i = 6; i <= 7; i++) {
+
+    for (let k = 1; k <= 9; k++) {
+
+        output11.innerHTML += `${i} * ${k} = ${i * k}`;
+
+        output11.innerHTML += '<br>';
+    }
+
+    output11.innerHTML += '<br>';
+
+}
+
+
+
+/* Task 12
+**  С помощью вложенных циклов выведите:
+**                                      1
+**                                      12
+**                                      123
+**                                      1234
+**                                      12345
+**
+*/
+
+
+for (let i = 1; i <= 5; i++) {
+
+    for (let k = 1; k <= i; k++) {
+
+        output12.innerHTML += k;
+
+    }
+
+    output12.innerHTML += '<br>';
+
+}
+
+/* Task 13
+**  С помощью вложенных циклов выведите:
+**                                      01 02 03 04 05 06 07 08 09 10
+**                                      11 12 13 14 15 16 17 18 19 20
+**                                      21 22 23 24 25 26 27 28 29 30
+**                                      31 32 33 34 35 36 37 38 39 40
+**                                      41 42 43 44 45 46 47 48 49 50
+**
+*/
+
+for (let i = 0; i < 5; i++) {
+
+    for (let k = 1; k <= 10; k++) {
+
+        if (k == 10) {
+
+            output13.innerHTML += `${i + 1}0`;
+
+        } else {
+
+            output13.innerHTML += `${i}${k}&nbsp;`;
+
+        }
+
+    }
+
+    output13.innerHTML += '<br>';
+
+}
+
+
+/* Task 14
+**  С помощью вложенных циклов выведите:
+**                                      54321
+**                                      4321
+**                                      321
+**                                      21
+**                                      1
+**
+*/
+
+
+for (let i = 5; i >= 1; i--) {
+
+    for (let k = i; k >= 1; k--) {
+
+        output14.innerHTML += k;
+
+    }
+
+    output14.innerHTML += '<br>';
+
+}
+
+
+/* Task 15
+**  С помощью вложенных циклов выведите:
+**                                      x x x x 1
+**                                      x x x 2 1
+**                                      x x 3 2 1
+**                                      x 4 3 2 1
+**                                      5 4 3 2 1
+**
+*/
+
+for (let i = 1; i <= 5; i++) {
+
+    for (let k = 5; k > i; k--) {
+
+        output15.innerHTML += 'x&nbsp;';
+
+    }
+
+    for (let k = i; k >= 1; k--) {
+
+        output15.innerHTML += `${k}&nbsp;`;
+
+    }
+
+    output15.innerHTML += '<br>';
+
+}
+
+/* Task 16
+**  С помощью вложенных циклов выведите:
+**                                      1
+**                                      2 2
+**                                      3 3 3
+**                                      4 4 4 4
+**                                      5 5 5 5 5
+**
+*/
+
+
+for (let i = 1; i <= 5; i++) {
+
+    for (let k = i; k >= 1; k--) {
+
+        output16.innerHTML += `${i}&nbsp;`;
+
+    }
+
+    output16.innerHTML += '<br>';
+
+}
+
+
+/* Task 17
+**  С помощью вложенных циклов выведите:
+**                                      5
+**                                      4 4
+**                                      3 3 3
+**                                      2 2 2 2
+**                                      1 1 1 1 1
+**
+*/
+
+for (let i = 5; i >= 1; i--) {
+
+    for (let j = i; j <= 5; j++) {
+
+        output17.innerHTML += `${i}&nbsp;`;
+
+    }
+
+    output17.innerHTML += '<br>';
+
+}
+/* Task 18
+**  С помощью вложенных циклов выведите:
+**                                      5
+**                                      x x
+**                                      3 3 3
+**                                      x x x x
+**                                      1 1 1 1 1
+**
+*/
+
+for (let i = 5; i >= 1; i--) {
+
+    if (i % 2 == 0) {
+
+        for (let j = i; j <= 5; j++) {
+
+            output18.innerHTML += `x&nbsp;`;
+
+        }
+
+    } else {
+
+
+        for (let j = i; j <= 5; j++) {
+
+            output18.innerHTML += `${i}&nbsp;`;
+
+        }
+
+    }
+
+    output18.innerHTML += '<br>';
+
+}
+
+/* Task 19
+**  С помощью вложенных циклов и символа * нарисуйте:
+**                                                     *****
+**                                                    *******
+**                                                   *********
+**
+*/
+
+for (let i = 0; i < 3; i++) {
+
+    for (let k = 0; k <= i + 4; k++) {
+
+        output19.innerHTML += '*';
+
+    }
+
+    output19.innerHTML += '<br>';
+}
+
+/* Task 20
+**  С помощью вложенных циклов и символа * нарисуйте:
+**                                                     **
+**                                                    ****
+**                                                   ******
+**                                                    ****
+**                                                     **
+**
+*/
+
+for (let i = 1; i <= 3; i++) {
+
+    for (let k = 0; k < i * 2; k++) {
+
+        output20.innerHTML += '*';
+
+    }
+
+    output20.innerHTML += '<br>';
+
+}
+for (let i = 2; i >= 1; i--) {
+
+    for (let k = 0; k < i * 2; k++) {
+
+        output20.innerHTML += '*';
+
+    }
+
+    output20.innerHTML += '<br>';
 
 }
